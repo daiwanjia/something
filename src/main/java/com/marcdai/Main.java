98c3fd071f2a2c539acc703d91e14b8eaa18e9d6
marcdai <marcdai@tencent.com>
2024-11-18 17:27:09 +0800

    init

---
+++ src/main/java/com/marcdai/Main.java
@@ -0,0 +1,11 @@
+package com.marcdai;
+
+/**
+ * @author：marcdai
+ * @date：2024/11/18
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
